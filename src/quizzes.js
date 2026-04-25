56b2f04a0644ee323f9f86d5331d5e4b4cfa0cb6
s750083-spec <o.savchenko.mail@gmail.com>
2026-04-25 20:18:04 +0300

    q_4.4.2

--- src/quizzes.js
+++ src/quizzes.js
@@ -1,125 +1,125 @@
 export const quizzes = {
-  "git-basics": {
+  "1": {
     title: "Основи Git",
-	timeLimit: 180, // 3 хвилини на весь тест
+    timeLimit: 180, // 3 хвилини на весь тест
     questions: [
-      { 
-        question: "Яка команда ініціалізує репозиторій?", 
-        options: ["git init", "git start"], 
-        answer: "git init" 
+      {
+        question: "Яка команда ініціалізує репозиторій?",
+        options: ["git init", "git start"],
+        answer: "git init"
       },
-      { 
-        question: "Як перевірити статус файлів?", 
-        options: ["git status", "git check"], 
-        answer: "git status" 
+      {
+        question: "Як перевірити статус файлів?",
+        options: ["git status", "git check"],
+        answer: "git status"
       },
-      { 
-        question: "Як перевірити статус файлів?", 
-        options: ["git status", "git check"], 
-        answer: "git status" 
+      {
+        question: "Як перевірити статус файлів?",
+        options: ["git status", "git check"],
+        answer: "git status"
       },
-      { 
-        question: "Як перевірити статус файлів?", 
-        options: ["git status", "git check"], 
-        answer: "git status" 
+      {
+        question: "Як перевірити статус файлів?",
+        options: ["git status", "git check"],
+        answer: "git status"
       },
-      { 
-        question: "Як перевірити статус файлів?", 
-        options: ["git status", "git check"], 
-        answer: "git status" 
+      {
+        question: "Як перевірити статус файлів?",
+        options: ["git status", "git check"],
+        answer: "git status"
       }
     ]
   },
-  "html-intro": {
+  "2": {
     title: "Вступ в HTML",
-	timeLimit: 180, // 3 хвилини на весь тест
+    timeLimit: 180, // 3 хвилини на весь тест
     questions: [
-      { 
-        question: "Що таке <a>?", 
-        options: ["Посилання", "Картинка"], 
-        answer: "Посилання" 
+      {
+        question: "Що таке <a>?",
+        options: ["Посилання", "Картинка"],
+        answer: "Посилання"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       }
     ]
   },
-  "html-intro_1": {
+  "3": {
     title: "Тема 1. Заняття 1.",
-	timeLimit: 180, // 3 хвилини на весь тест
+    timeLimit: 180, // 3 хвилини на весь тест
     questions: [
-      { 
-        question: "Що таке <a>?", 
-        options: ["Посилання", "Картинка"], 
-        answer: "Посилання" 
+      {
+        question: "Що таке <a>?",
+        options: ["Посилання", "Картинка"],
+        answer: "Посилання"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       }
     ]
   },
-  "html-intro_2": {
+  "4": {
     title: "Тема 1. Заняття 2.",
-	timeLimit: 180, // 3 хвилини на весь тест
+    timeLimit: 180, // 3 хвилини на весь тест
     questions: [
-      { 
-        question: "Що таке <a>?", 
-        options: ["Посилання", "Картинка"], 
-        answer: "Посилання" 
+      {
+        question: "Що таке <a>?",
+        options: ["Посилання", "Картинка"],
+        answer: "Посилання"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       },
-      { 
-        question: "Який тег створює заголовок першого рівня?", 
-        options: ["<h1>", "<header>"], 
-        answer: "<h1>" 
+      {
+        question: "Який тег створює заголовок першого рівня?",
+        options: ["<h1>", "<header>"],
+        answer: "<h1>"
       }
     ]
   }
